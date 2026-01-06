38de54c802fb1ae823cbb15b3f8de88a6cb7f151
Felippe Nardi <wdfelippe@gmail.com>
2026-01-06 15:18:42 -0300

    Update things

---
+++ HeartRateTracker/HeartRateTracker Watch App/HeartRateTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartRateTrackerApp.swift
+//  HeartRateTracker Watch App
+//
+//  Created by Felippe Nardi on 1/6/26.
+//
+
+import SwiftUI
+
+@main
+struct HeartRateTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
